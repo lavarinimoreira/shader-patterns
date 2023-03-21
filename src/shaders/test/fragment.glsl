d13ac1ada07f5e3f2ca415ad97674b6cc5d7f0d8
Gabriel Lavarini <gabriel@lavarinimoreira.me>
2023-03-20 21:58:48 -0300

    shader-patterns-component

--- src/shaders/test/fragment.glsl
+++ src/shaders/test/fragment.glsl
@@ -1,28 +1,6 @@
-//Solid red color:
-
-precision mediump float;
-
-uniform vec3 uColor;
-uniform sampler2D uTexture;
-
 varying vec2 vUv;
 
-void main(){
-
-    vec4 textureColor = texture2D(uTexture, vUv);
-
-    //                 (r, g, b, transparency);
-    // gl_FragColor = vec4(uColor,1.0);
-
-    gl_FragColor = textureColor;
+void main()
+{
+    gl_FragColor = vec4(0.5, 0.0, 1.0, 1.0);
 }
-
-
-// In this example, the shader creates a color based on the sine of the time.
-// precision highp float;
-//       varying vec2 uv;
-//       uniform float time;
-//       void main() {
-//         gl_FragColor = vec4(vec3(sin(time)), 1.0);
-//       }
-
