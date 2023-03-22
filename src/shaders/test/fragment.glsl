05675fd9fc121d0d4a094c7d6e358ea0a599af97
Gabriel Lavarini <gabriel@lavarinimoreira.me>
2023-03-22 18:56:47 -0300

    adding patterns

--- src/shaders/test/fragment.glsl
+++ src/shaders/test/fragment.glsl
@@ -37,10 +37,45 @@ void main()
     // float strength = step(0.8, mod(vUv.x * 10.0, 1.0));
     // strength *= step(0.8, mod(vUv.y * 10.0, 1.0));
 
-     //Pattern 13
-    float strength = step(0.4, mod(vUv.x * 10.0, 1.0));
-    strength *= step(0.8, mod(vUv.y * 10.0, 1.0));
+    //  //Pattern 13
+    // float strength = step(0.4, mod(vUv.x * 10.0, 1.0));
+    // strength *= step(0.8, mod(vUv.y * 10.0, 1.0));
+
+    //  //Pattern 14
+    // float barX = step(0.4, mod(vUv.x * 10.0, 1.0));
+    // barX *= step(0.8, mod(vUv.y * 10.0, 1.0));
+    // float barY = step(0.4, mod(vUv.y * 10.0, 1.0));
+    // barY *= step(0.8, mod(vUv.x * 10.0, 1.0));
+    // float strength = barX + barY;
+
+    //  //Pattern 15
+    // float barX = step(0.4, mod(vUv.x * 10.0, 1.0));
+    // barX *= step(0.8, mod(vUv.y * 10.0 + 0.2, 1.0));
+    // float barY = step(0.4, mod(vUv.y * 10.0, 1.0));
+    // barY *= step(0.8, mod(vUv.x * 10.0 + 0.2, 1.0));
+    // float strength = barX + barY;
+
+    // // Pattern 16
+    // float strength = abs(vUv.x - 0.5);
+
+    // // // Pattern 17
+    // float strength = min(abs(vUv.x - 0.5), abs(vUv.y - 0.5));
+
+    // // Pattern 18
+    // float strength = max(abs(vUv.x - 0.5), abs(vUv.y - 0.5));
+
+
+    // // Pattern 19
+    // float strength = step(0.2, max(abs(vUv.x - 0.5), abs(vUv.y - 0.5)));
+
+    // Pattern 19
+    float square1 = step(0.2, max(abs(vUv.x - 0.5), abs(vUv.y - 0.5)));
+    float square2 = 1.0 - step(0.25, max(abs(vUv.x - 0.5), abs(vUv.y - 0.5)));
+    float strength = square1 * square2;
+
+    // // Tests
+    // float strength = vUv.x + vUv.y;
 
     // gl_FragColor = vec4(vUv, 0.6, 1.0);
-    gl_FragColor = vec4(strength, strength,strength, 1.0); 
+    gl_FragColor = vec4(strength, strength, strength, 1.0); 
 }
