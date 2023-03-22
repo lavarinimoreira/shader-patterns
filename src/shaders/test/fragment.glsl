edf6df3686477a92868827f0eb01dd29d9c12f94
Gabriel Lavarini <gabriel@lavarinimoreira.me>
2023-03-22 20:05:59 -0300

    adding patterns

--- src/shaders/test/fragment.glsl
+++ src/shaders/test/fragment.glsl
@@ -1,5 +1,20 @@
 varying vec2 vUv;
 
+#define PI 3.1415926535897932384626433832795
+
+float random(vec2 st)
+{
+    return fract(sin(dot(st.xy, vec2(12.9898,78.233))) * 43758.5453123);
+}
+
+vec2 rotate(vec2 uv, float rotation, vec2 mid)
+{
+    return vec2(
+      cos(rotation) * (uv.x - mid.x) + sin(rotation) * (uv.y - mid.y) + mid.x,
+      cos(rotation) * (uv.y - mid.y) - sin(rotation) * (uv.x - mid.x) + mid.y
+    );
+}
+
 void main()
 {
     // // Pattern 3
@@ -64,15 +79,67 @@ void main()
     // // Pattern 18
     // float strength = max(abs(vUv.x - 0.5), abs(vUv.y - 0.5));
 
-
     // // Pattern 19
     // float strength = step(0.2, max(abs(vUv.x - 0.5), abs(vUv.y - 0.5)));
 
-    // Pattern 19
-    float square1 = step(0.2, max(abs(vUv.x - 0.5), abs(vUv.y - 0.5)));
-    float square2 = 1.0 - step(0.25, max(abs(vUv.x - 0.5), abs(vUv.y - 0.5)));
-    float strength = square1 * square2;
+    // // Pattern 20
+    // float square1 = step(0.2, max(abs(vUv.x - 0.5), abs(vUv.y - 0.5)));
+    // float square2 = 1.0 - step(0.25, max(abs(vUv.x - 0.5), abs(vUv.y - 0.5)));
+    // float strength = square1 * square2;
 
+    //  // Pattern 21
+    // float strength = floor(vUv.x * 10.0) / 10.0;
+
+    //  // Pattern 22
+    // float strength = floor(vUv.x * 10.0) / 10.0;
+    // strength *= floor(vUv.y * 10.0) / 10.0;
+
+    // // Pattern 23
+    // float strength = random(vUv);
+
+    //  // Pattern 24
+    // vec2 gridUv = vec2(
+    //     floor(vUv.x * 10.0) / 10.0,
+    //     floor(vUv.y * 10.0) / 10.0
+    // );
+    // float strength = random(gridUv);
+
+    // // Pattern 25
+    // vec2 gridUv = vec2(
+    //     floor(vUv.x * 10.0) / 10.0,
+    //     floor((vUv.y + vUv.x * 0.5) * 10.0) / 10.0
+    // );
+    // float strength = random(gridUv);
+
+    // // Pattern 26
+    // float strength = length(vUv);
+
+    // // Pattern 27
+    // //float strength = length(vUv - 0.5); // first way to do it.
+    // float strength = distance(vUv, vec2(0.5, 0.5));
+
+    //  // Pattern 28
+    // float strength = 1.0 - distance(vUv, vec2(0.5, 0.5));
+
+    // // Pattern 29
+    // float strength = 0.015 / distance(vUv, vec2(0.5, 0.5));
+
+    // // Pattern 30
+    // vec2 lightUv = vec2(
+    //     vUv.x * 0.1 + 0.45,
+    //     vUv.y * 0.5 + 0.25
+    // );
+    // float strength = 0.015 / distance(lightUv, vec2(0.5, 0.5));
+
+    // Pattern 31
+    // float strength = 0.15 / (distance(vec2(vUv.x, (vUv.y - 0.5) * 5.0 + 0.5), vec2(0.5)));
+    // strength *= 0.15 / (distance(vec2(vUv.y, (vUv.x - 0.5) * 5.0 + 0.5), vec2(0.5)));
+
+    // Pattern 32
+    vec2 rotatedUv = rotate(vUv, PI * 0.25, vec2(0.5));
+    float strength = 0.15 / (distance(vec2(rotatedUv.x, (rotatedUv.y - 0.5) * 5.0 + 0.5), vec2(0.5)));
+    strength *= 0.15 / (distance(vec2(rotatedUv.y, (rotatedUv.x - 0.5) * 5.0 + 0.5), vec2(0.5)));
+    
     // // Tests
     // float strength = vUv.x + vUv.y;
 
